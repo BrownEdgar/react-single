f8c7f1b5a67602ea17821b6c44433fb66d610e82
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-06 13:55:18 +0400

    add my first HM (demo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1)
+  }
   return (
     <div className='App'>
-      <h1>React-single</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={handleClick}>change count</button>
     </div>
   )
 }
