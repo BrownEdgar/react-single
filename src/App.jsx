e5716d62f4f28fe560a5bfbc7fc8d54a37f5b667
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-11 13:22:20 +0400

    addede condition text

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,54 @@ import { useState } from 'react'
 import "./App.css"
 
 export default function App() {
-  const [blogs, setBlogs] = useState([
+  const [blogs] = useState([
     {
       id: 1,
       heading: "Aliquam Hendrerit Mi Metus",
-      info: "It is with great pleasure that I speak to the I've experienced with the Cantrell Brothers and the team at..."
+      info: "It is with great pleasure that I speak to the I've experienced with the Cantrell Brothers and the team at Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit architecto maxime blanditiis, delectus accusantium similique sunt fuga distinctio dicta eos. Vitae accusamus suscipit hic architecto, commodi blanditiis totam nemo deleniti!",
     },
     {
       id: 2,
       heading: "Mauris Vulputate Cras Amet",
-      info: "It is with great pleasure that I speak to the I've experienced with the Cantrell Brothers and the team at..."
+      info: "It is with great pleasure that I speak to the I've experienced with the Cantrell Brothers and the team at Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit architecto maxime blanditiis, delectus accusantium similique sunt fuga distinctio dicta eos. Vitae accusamus suscipit hic architecto, commodi blanditiis totam nemo deleniti!",
     },
     {
       id: 3,
       heading: "Suspendisse Nullam Sodales",
-      info: "It is with great pleasure that I speak to the I've experienced with the Cantrell Brothers and the team at..."
+      info: "It is with great pleasure that I speak to the I've experienced with the Cantrell Brothers and the team at Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit architecto maxime blanditiis, delectus accusantium similique sunt fuga distinctio dicta eos. Vitae accusamus suscipit hic architecto, commodi blanditiis totam nemo deleniti!",
     },
   ]);
+
+  const [currentId, setCurrentId] = useState(null)
+
+  const handleClick = (id) => {
+    if (currentId === id) {
+      setCurrentId(null)
+    } else {
+      setCurrentId(id)
+    }
+  }
+
   return (
     <div className='App'>
       <h1>Our Blog</h1>
       <div className='Blogs'>
         {
-          blogs.map(elem=>{
-            return(
+          blogs.map(elem => {
+            return (
               <div key={elem.id}>
                 <h5>AXONVIP | 01 AUG, 2019</h5>
                 <h3>{elem.heading}</h3>
-                <p>{elem.info}</p>
-                <button>READ MORE</button>
+                <p className={`${(currentId === elem.id) ? 'active' : ''}`}>
+                  {
+                    (currentId === elem.id)
+                      ? elem.info
+                      : elem.info.slice(0, 110).concat('...')
+                  }
+                </p>
+                <button onClick={() => handleClick(elem.id)}>
+                  {(currentId === elem.id) ? 'HIDE' : 'READ MORE'}
+                </button>
               </div>
             )
           })
