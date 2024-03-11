699dd800c43b7ad2701507574135e6adb0800e63
Ishkhan05 <g.i.h.a.s@bk.ru>
2024-03-11 11:17:05 +0400

    homework 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import { useState } from 'react'
+import "./App.css"
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    setCount(count + 1)
-  }
+  const [blogs, setBlogs] = useState([
+    {
+      id: 1,
+      heading: "Aliquam Hendrerit Mi Metus",
+      info: "It is with great pleasure that I speak to the I've experienced with the Cantrell Brothers and the team at..."
+    },
+    {
+      id: 2,
+      heading: "Mauris Vulputate Cras Amet",
+      info: "It is with great pleasure that I speak to the I've experienced with the Cantrell Brothers and the team at..."
+    },
+    {
+      id: 3,
+      heading: "Suspendisse Nullam Sodales",
+      info: "It is with great pleasure that I speak to the I've experienced with the Cantrell Brothers and the team at..."
+    },
+  ]);
   return (
     <div className='App'>
-      <h1>Count: {count}</h1>
-      <button onClick={handleClick}>change count</button>
+      <h1>Our Blog</h1>
+      <div className='Blogs'>
+        {
+          blogs.map(elem=>{
+            return(
+              <div key={elem.id}>
+                <h5>AXONVIP | 01 AUG, 2019</h5>
+                <h3>{elem.heading}</h3>
+                <p>{elem.info}</p>
+                <button>READ MORE</button>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
