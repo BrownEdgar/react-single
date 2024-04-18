cfe092db70b82dd433e0f62d95ba6db694f455fb
Ishkhan05 <g.i.h.a.s@bk.ru>
2024-04-18 12:08:25 +0400

    pagination homework done

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -1,38 +1,40 @@
 import React, { useState, useEffect, useReducer } from 'react'
 import blogReducer, { initialState } from './blogReducer'
-import { Link, useSubmit } from 'react-router-dom'
-import './Work.scss'
-import axios from 'axios'
+import { Link } from 'react-router-dom'
 import DisplayDate from '../../components/DisplayDate/DisplayDate'
-import Pagination from '../../components/Pagination/Pagination'
+import ReactPaginate from 'react-paginate'
+import axios from 'axios'
+import './Work.scss'
 
 export default function Work() {
   const [state, dispatch] = useReducer(blogReducer, initialState)
-  
-  
   const [currentPage, setCurrentPage] = useState(1)
   const [postsPerPage, setPostsPerPage] = useState(9)
   const [loading, setLoading] = useState(false)
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFistPost = indexOfLastPost - postsPerPage
-  const currentPosts = state.slice(indexOfFistPost, indexOfLastPost)
   
   useEffect(() => {
     setLoading(true)
-    axios(import.meta.env.VITE_DB_URL)
+    axios(import.meta.env.VITE_DB_URL,{
+      params: {
+        _limit: postsPerPage,
+        _start: (currentPage* postsPerPage) - postsPerPage
+      }
+    })
     .then(res => dispatch({ type: "add_blogs", payload: res.data }))
     .finally(()=>{
       setTimeout(setLoading, 600, false);
     })
   }, [currentPage,postsPerPage])
-  const paginate = (pageNumber) =>{setCurrentPage(pageNumber)}
+
+  const handlePageClick = ({selected}) =>{setCurrentPage(selected + 1)}
+
   return (
     <div className='Work'>
       <div className="Work__header">
         <h1>OUR BLOG</h1>
       </div>
       <div className="Work__wrapper">
-        {currentPosts.map(elem => {
+        {state.map(elem => {
           return (
             <Link key={elem.id} to={`${elem.id}`}>
               <img src={elem.poster} />
@@ -42,10 +44,15 @@ export default function Work() {
           )
         })}
       </div>
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={state.length}
-        paginate={paginate}
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel=">"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={Math.ceil(50 / postsPerPage)}
+        previousLabel="<"
+        renderOnZeroPageCount={null}
+        className='Work__list'
       />
     </div>
   )
