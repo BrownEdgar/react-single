26c248d59b30239ed5f633b1d3ef3cbee10dafef
Ishkhan05 <g.i.h.a.s@bk.ru>
2024-04-18 11:21:32 +0400

    ....

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,33 +1,31 @@
 import React, { useState } from 'react'
 import "./Pagination.scss"
+import classNames from "classnames"
 
-export default function Pagination({postsPerPage, totalPosts, paginate}) {
-  const pageNumbers = [];
-  for (let i = 0; i < Math.ceil(totalPosts/postsPerPage); i++) {
-    pageNumbers.push(i+1)
-  }
-  const [activePage, setActivePage] = useState(1)
-
-  const handlePageClick = (number) =>{
-    setActivePage(number)
-    paginate(number)
-  }
+export default function Pagination({postsPerPage, totalPosts, paginate, page}) {
   
   return (
     <div className='Pagination'>
       <nav>
         <ul>
           {
-            pageNumbers.map((number,index)=>{
-              return(
-                <li key={index} onClick={()=>{
-                  handlePageClick(number)
-                  paginate(number)
-                }}>
-                  {number}
-                </li>
-              )
-            })
+            new Array(Math.ceil(totalPosts / postsPerPage))
+              .fill()
+              .map((_,index)=>{
+                return(
+                  <li key={index}>
+                    <a href="#"
+                      onClick={()=>paginate(index + 1)}
+                      className={classNames({
+                        active: index == page - 1
+                      })}
+                    
+                    >
+                      {index + 1}
+                    </a>
+                  </li>
+                )
+              })
           }
         </ul>
       </nav>
