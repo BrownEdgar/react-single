94cc356a58c7b36cec4fbaf77c00ac1893e4327f
Ishkhan05 <g.i.h.a.s@bk.ru>
2024-04-26 10:24:25 +0400

    done

--- src/homeworks/usecontext/useArray.jsx
+++ src/homeworks/usecontext/useArray.jsx
@@ -3,10 +3,10 @@ import React, { useState } from 'react'
 export default function useArray(Arr) {
   const [array, setArray] = useState(Arr)
   const push = (a) =>setArray([...array, a])
-  const filter = () =>setArray(array.filter(n => n <= 3))
-  const remove = (n) =>setArray(array.slice(0,n).concat(array.slice(-(array.length-n-1))))
+  const filter = (cb) =>setArray(array.filter(cb))
+  const remove = (n) =>setArray(array.toSpliced(n, 1))
   const clear = () =>setArray([])
-  const set = ([n,m])=>setArray([n,m])
-  const update = (x,y) =>setArray([array.fill(y,x,x+1)])
+  const set = (arr)=>setArray(arr)
+  const update = (x,y) =>setArray(array.with(x,y))
   return ({array, push, filter, remove, clear, set, update })
 }
